6ca76f44d3f3423f9e01b55a834d3d70de749e2e
Digita1Panda <prodigykyo@gmail.com>
2024-01-05 20:15:28 +0000

    starter code and prompt function

---
+++ script.js
@@ -0,0 +1,138 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+let lowercase;
+let uppercase;
+let numeric;
+let specialChar;
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  charSet = prompt(`How many characters do you want your password to contain?`);
+  charSet = Number(charSet);
+  if (charSet >= 8 && charSet <= 128) {
+    lowercase = confirm(
+      `Do you want your password to have lower case letters?`
+    );
+    uppercase = confirm(
+      `Do you want your password to contain upper case letters?`
+    );
+    numeric = confirm(`Do you want your password to contain numerals?`);
+    specialChar = confirm(
+      `Do you want your password to contain special letters?`
+    );
+  } else {
+    alert(
+      `Your password should be no less than 8 characters or more than 128 characters long.`
+    );
+  }
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {}
+
+// Function to generate password with user input
+function generatePassword() {}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+getPasswordOptions();
